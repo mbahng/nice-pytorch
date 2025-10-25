185b68ddef8dba09275399489516274a74914c55
mbahng <bahngmc@gmail.com>
2025-10-24 22:36:00 -0400

    updates

--- traintest.py
+++ traintest.py
@@ -12,7 +12,7 @@ def train_nice(model, dataloader, optimizer, device):
     x = batch[0].to(device)
     x = x.view(x.size(0), *model.idim)
     x += torch.rand_like(x) / 256
-    x += torch.clamp(x, 0, 1)
+    x = torch.clamp(x, 0, 1)
 
     z, log_likelihood = model(x) 
 
@@ -30,14 +30,14 @@ def train_realnvp(model, dataloader, optimizer, device):
   model.train()
   total_loss = 0.0
 
-  for batch in tqdm(dataloader): 
+  for batch in tqdm(dataloader):
     x = batch[0].to(device)
 
     # This should be included for discrete data
     x += torch.rand_like(x) / 256
-    x += torch.clamp(x, 0, 1)
+    x = torch.clamp(x, 0, 1)
 
-    z, log_likelihood = model(x) 
+    z, log_likelihood = model(x)
 
     loss = -torch.mean(log_likelihood)
     total_loss += loss.item()
